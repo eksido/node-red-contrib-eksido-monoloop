2a01479547430199836cb70df4d24babfe87e4ae
Franz Malten Buemann <fmb@eksido.com>
2020-07-07 17:16:45 +0200

    Update config

--- monoloop.js
+++ monoloop.js
@@ -11,3 +11,23 @@ module.exports = function(RED) {
   RED.nodes.registerType("monoloop-config", MonoloopConfigNode)
 
 }
+
+function MonoloopProfile(config) {
+  RED.nodes.createNode(this,config);
+
+  // Retrieve the config node
+  this.accountid = RED.nodes.getNode(config.accountid);
+  this.apikey = RED.nodes.getNode(config.apikey);
+
+  if (this.accountid && this.apikey) {
+      // Do something with:
+      //  this.server.host
+      //  this.server.port
+  } else {
+      // No config node configured
+  }
+}
+
+RED.nodes.registerType("monoloop-profile",MonoloopProfile);
+
+
