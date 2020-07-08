89d15a5e024bcb572300cfd2457797d911534246
Franz Malten Buemann <fmb@eksido.com>
2020-07-08 15:26:07 +0200

    Update

--- monoloop.js
+++ monoloop.js
@@ -1,33 +1,48 @@
 // monoloop.js
 module.exports = function(RED) {
+  var request = require('request')
 
+  // Configuration Node 
   function MonoloopConfigNode(n) {
     RED.nodes.createNode(this, n);
     this.name = n.name
-    this.accountid = config.accountid
-    this.apikey = config.apikey
+    this.accountid = n.accountid
+    this.apikey = n.apikey
   }
-
   RED.nodes.registerType("monoloop-config", MonoloopConfigNode)
+  
+  // Profile Node
+  function MonoloopProfile(n) {
+    RED.nodes.createNode(this,n);
+    let node = this;
+    this.credentials = RED.nodes.getNode(n.configuration)
+    this.name = n.name;
+    this.action = n.action;
 
-}
+    // Retrieve the config node
+    this.accountid = this.credentials.accountid;
+    this.apikey = this.credentials.apikey;
 
-function MonoloopProfile(config) {
-  RED.nodes.createNode(this,config);
+    //Create URL
+    let url = 'https://'+this.accountid+':'+this.apikey+'@'+this.accountid+'invoke.monoloop.com/V1/profiles/5ee74b9c5e9825220529785c'
 
-  // Retrieve the config node
-  this.accountid = RED.nodes.getNode(config.accountid);
-  this.apikey = RED.nodes.getNode(config.apikey);
-
-  if (this.accountid && this.apikey) {
-      // Do something with:
-      //  this.server.host
-      //  this.server.port
-  } else {
-      // No config node configured
+    // Check configuration exists 
+    if (this.accountid && this.apikey) {
+      node.on('input', function(msg) {
+        request(
+          {
+              url : url
+          },
+          function (error, response, body) {
+            msg.payload = response;
+            node.send(msg);
+          }
+        )
+      })
+    } else {
+      node.log("Configuration for Monoloop Node was not provided");
+    }
   }
-}
+  RED.nodes.registerType("monoloop-profile", MonoloopProfile);
 
-RED.nodes.registerType("monoloop-profile",MonoloopProfile);
-
-
+} // End export
