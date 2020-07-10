4875fdd4a75fe3f06bcf99d8dbbead152b3102fd
Franz Malten Buemann <fmb@eksido.com>
2020-07-10 09:53:22 +0200

    Update

--- monoloop.js
+++ monoloop.js
@@ -9,7 +9,7 @@ module.exports = function(RED) {
     this.accountid = n.accountid
     this.apikey = n.apikey
   }
-  RED.nodes.registerType("monoloop-config", MonoloopConfigNode)
+  RED.nodes.registerType("monoloop-config", MonoloopConfigNode )
   
   // Profile Node
   function MonoloopProfile(n) {
@@ -23,26 +23,47 @@ module.exports = function(RED) {
     this.accountid = this.credentials.accountid;
     this.apikey = this.credentials.apikey;
 
-    //Create URL
-    let url = 'https://'+this.accountid+':'+this.apikey+'@'+this.accountid+'invoke.monoloop.com/V1/profiles/5ee74b9c5e9825220529785c'
-
     // Check configuration exists 
     if (this.accountid && this.apikey) {
-      node.on('input', function(msg) {
-        request(
-          {
-              url : url
-          },
-          function (error, response, body) {
-            msg.payload = response;
-            node.send(msg);
-          }
-        )
-      })
+      // Find single profiles 
+      if ( this.action === "getprofile" ){
+        // Request URL
+        let url = 'https://'+this.accountid+':'+this.apikey+'@'+this.accountid+'invoke.monoloop.com/V1/profiles/5ee74b9c5e9825220529785c'
+        node.on('input', function(msg) {
+          request(
+            {
+                url : url
+            },
+            function (error, response, body) {
+              msg.payload = response.body;
+              node.send(msg);
+            }
+          )
+        })
+      } else if ( this.action === "getprofiles" ) { // Find latest profiles 
+        let url = 'https://'+this.accountid+':'+this.apikey+'@api.monoloop.com/profiles/'
+        node.on('input', function(msg) {
+          request(
+            {
+                url : url
+            },
+            function (error, response, body) {
+              msg.payload = response.body;
+              node.send(msg);
+            }
+          )
+        })
+
+      } else if ( this.action === "updateprofile" ) { // Find latest profiles 
+
+      } else if ( this.action === "delete" ) {
+
+      } else { // No action recorded  
+        node.log("Action for Monoloop Profile Node was not provided")
+      }    
     } else {
-      node.log("Configuration for Monoloop Node was not provided");
+      node.log("Configuration for Monoloop Node was not provided")
     }
   }
   RED.nodes.registerType("monoloop-profile", MonoloopProfile);
-
 } // End export
